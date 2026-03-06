247014301ecd52087d4ca25e57584f414a5600da
DGLocSon <Omonguyen0@gmail.com>
2026-03-06 23:01:40 +0700

    feat: update AuthController

--- src/main/java/com/nhom4/demo/Controller/AuthController.java
+++ src/main/java/com/nhom4/demo/Controller/AuthController.java
@@ -1,5 +1,16 @@
 package com.nhom4.demo.Controller;
 
 public class AuthController {
-    
+    public String login() {
+        return "login";
+    }
+    public String loginGoogle() {
+        return "login-google";
+    }
+    public String loginFacebook() {
+        return "login-facebook";
+    }
+    public String loginGithub() {
+        return "login-github";
+    }
 }
